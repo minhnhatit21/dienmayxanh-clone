7d4c6cfa3a70fa909315b56ffa7b1d7f42f37e76
minhnhatit21 <nhmnhat2101.it@gmail.com>
2024-04-08 16:33:28 +0700

    add service-covinient block, category-trend block, footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
-    $(".header-content__category").hover(function() {
+    $(".header-content__category").hover(function () {
         $(this).parent().find(".catergoty__dropdown").css("display", "flex");
-    }, function() {
+    }, function () {
         $(this).parent().find(".catergoty__dropdown").css("display", "none");
     });
 
-    $(".catergoty__dropdown__main-menu li").hover(function() {
+    $(".catergoty__dropdown__main-menu li").hover(function () {
         $(this).parent().find(".submenu").css("display", "flex");
-    }, function() {
+    }, function () {
         $(this).parent().find(".submenu").css("display", "none");
     });
+
 })
