f01b769cff8b29dbf3ccc2a6a8722e1e60220b1d
minhnhatit21 <nhmnhat2101.it@gmail.com>
2024-04-09 15:23:48 +0700

    add Product promo 7 day block, two sidebar banner, footer

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,48 @@ $(document).ready(function () {
         $(this).parent().find(".submenu").css("display", "none");
     });
 
+    // Product Promo 7 days tab
+
+    $(".tab-item__link").click(function (e) {
+        e.preventDefault();
+        var $parent = $(this).parent();
+
+        console.log("Parent: ", $parent);
+
+        if(!$parent.hasClass("active-tab")) {
+            $parent.siblings().removeClass("active-tab")
+            $parent.addClass("active-tab");
+
+            var prdPromoContentId = $(this).attr("href");
+            console.log("Product contentID: ", prdPromoContentId);
+            $(prdPromoContentId).removeClass("hidden");
+            $(prdPromoContentId).siblings().addClass("hidden");
+        }
+    })
+
+    //Footer Dropdown
+    $("footer .showtaga").click(function (e) {
+        e.preventDefault();
+
+        var $parent = $(this).parent().parent();
+        var $this = $(this);
+        console.log($parent)
+
+        if($parent.children().hasClass("hidden")) {
+            $parent.children().removeClass("hidden");
+            $this.addClass("hidden");
+        }
+    })
+
+    //Check Scroll Position
+    $(window).scroll(function (event) {
+        var scrollY = $(window).scrollTop();
+        if(scrollY >= 450) {
+            $('.sticky-sidebar').removeClass("hidden")
+        } else {
+            $('.sticky-sidebar').addClass("hidden")
+        }
+
+    });
+
 })
